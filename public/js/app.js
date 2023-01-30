e190fec2eba02f40825f8443aab75a75ffab5467
Kévin Vds <118352151+SiskaKizuato@users.noreply.github.com>
2023-01-30 17:01:07 +0100

    ajout dd Blog

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,4 @@
+// dropdown Latest
 const containerDdLatest = document.querySelector('.containerDdLatest');
 const btnLatest = document.querySelector('.btnLatest');
 
@@ -7,12 +8,30 @@ containerDdLatest.style.display = 'block';
 
 btnLatest.addEventListener('mouseout', function () {
     containerDdLatest.style.display = 'block';
-    });
+});
 
 containerDdLatest.addEventListener('mouseover', function () {
     containerDdLatest.style.display = 'block';
 });
 containerDdLatest.addEventListener('mouseout', function () {
-containerDdLatest.style.display = 'none';
+    containerDdLatest.style.display = 'none';
 });
 
+// dropdown Blog
+const containerDdBlog = document.querySelector('.containerDdBlog');
+const btnBlog = document.querySelector('.btnBlog');
+
+btnBlog.addEventListener('mouseover', function () {
+    containerDdBlog.style.display = 'block';
+});
+
+btnBlog.addEventListener('mouseout', function () {
+    containerDdBlog.style.display = 'block';
+});
+
+containerDdBlog.addEventListener('mouseover', function () {
+    containerDdBlog.style.display = 'block';
+});
+containerDdBlog.addEventListener('mouseout', function () {
+    containerDdBlog.style.display = 'none';
+});
