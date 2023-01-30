c4bdcbeefd85b58ba82b8b55f34b108ecbbbca9f
Kévin Vds <118352151+SiskaKizuato@users.noreply.github.com>
2023-01-30 16:48:25 +0100

    dropdown latest fonctionnel

---
+++ public/js/app.js
@@ -0,0 +1,18 @@
+const containerDdLatest = document.querySelector('.containerDdLatest');
+const btnLatest = document.querySelector('.btnLatest');
+
+btnLatest.addEventListener('mouseover', function () {
+containerDdLatest.style.display = 'block';
+});
+
+btnLatest.addEventListener('mouseout', function () {
+    containerDdLatest.style.display = 'block';
+    });
+
+containerDdLatest.addEventListener('mouseover', function () {
+    containerDdLatest.style.display = 'block';
+});
+containerDdLatest.addEventListener('mouseout', function () {
+containerDdLatest.style.display = 'none';
+});
+
