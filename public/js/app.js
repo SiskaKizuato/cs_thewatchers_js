776013e7ec8fd74bf78d8ed42557527f833646e6
Kévin Vds <118352151+SiskaKizuato@users.noreply.github.com>
2023-01-30 22:00:28 +0100

    ajout du bouton modes, issu du projet calcalculatrice, non fonctionnel pour le moment

--- public/js/app.js
+++ public/js/app.js
@@ -67,3 +67,34 @@ containerDdPages.addEventListener('mouseout', function () {
     containerDdPages.style.display = 'none';
 });
 
+
+// mode lightdark
+const barre = document.getElementsByClassName('barre')[0]
+
+const boule = document.getElementsByClassName('boule')[0]
+
+const paragraphs = document.getElementsByTagName('p');
+
+let modeBlanc = true;
+
+boule.addEventListener('click', () => {
+    modeBlanc = !modeBlanc;
+
+    document.body.style.backgroundColor = modeBlanc ? 'black' : 'white';
+    barre.style.backgroundColor = modeBlanc ? 'firebrick' : 'rgb(135, 206, 235)'
+    containerT.style.borderColor = modeBlanc ? 'firebrick' : 'rgb(135, 206, 235)'
+    containerT.style.borderWidth = modeBlanc ? '1px' : '1.5px'
+    barre.style.borderColor = modeBlanc ? 'firebrick' : 'black'
+    barre.style.justifyContent = modeBlanc ? 'right' : 'left'
+    boule.style.right = modeBlanc ? '-1px' : '1px'
+    input.style.backgroundColor = modeBlanc ? 'rgba(178, 34, 34, 0.765)' : 'rgb(135, 206, 235)'
+    input.style.boxShadow = modeBlanc ? '0 0 0 1px white' : '0 0 0 1px black';
+
+    for (let i = 0; i < paragraphs.length; i++) {
+        paragraphs[i].style.borderColor = modeBlanc ? 'white' : 'black'
+        paragraphs[i].style.borderWidth = modeBlanc ? '1px' : '1.5px'
+        paragraphs[i].style.color = modeBlanc ? 'firebrick' : 'black'
+        paragraphs[i].style.backgroundColor = modeBlanc ? 'black' : 'rgb(135, 206, 235)'
+    }
+    boule.style.float = modeBlanc ? 'right' : 'left'
+});
